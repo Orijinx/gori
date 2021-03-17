8ccb3e18937f5f5c3150588650cadc1d755b784c
Vladislav <vladonnx@mail.ru>
2021-03-18 02:26:14 +0300

    Update Readme

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-gori
+#Gori
+_Gori - Gojinx optimizer_
+##USAGE:
+gori [global options] command [command options] [arguments...]
+
+##VERSION:
+0.0.1
+
+##DESCRIPTION:
+A CLI application for Gorijinx-framework
+
+##AUTHOR:
+- Highjin <vladonnx@mail.ru>
+
+##COMMANDS:
+- create   Create a default project
+- help, h  Shows a list of commands or help for one command
+
+##GLOBAL OPTIONS:
+- --help, -h     show help (default: false)
+- --version, -v  print the version (default: false)
+
+##COPYRIGHT:
+(C) Orijinx 2021
--- gori.go
+++ gori.go
@@ -2,27 +2,49 @@ package main
 
 import (
 	"fmt"
+	git "github.com/go-git/go-git" // with go modules disabled
+	cli "github.com/urfave/cli"
 	"log"
 	"os"
-
-	"github.com/urfave/cli"
 )
 
 func main() {
+
 	app := cli.NewApp()
+
+	//Descriptions
+	app.Authors = []*cli.Author{
+		&cli.Author{Name: "Highjin", Email: "vladonnx@mail.ru"},
+	}
+	app.Copyright = "(C) Orijinx 2021"
+	app.Name = "Gori"
+	app.Version = "0.0.1"
+	app.Description = "A CLI application for Gorijinx-framework"
+	///////////////////////////////////////////////////////////////////////////
 
 	// we create our commands
 	app.Commands = []*cli.Command{
 		{
 			Name:  "create",
-			Usage: "Create a project",
+			Usage: "Create a default project",
 			// the action, or code that will be executed when
 			// we execute our `ns` command
 			Action: func(c *cli.Context) error {
 
 				fmt.Println(c.Args().Get(0))
 
+				Path := "./" + c.Args().Get(0)
 
+
+				_, err := git.PlainClone(Path, false, &git.CloneOptions{
+					SingleBranch: true,
+					RemoteName: "framework",
+					URL:      "https://github.com/Orijinx/Gorijinx",
+					Progress: os.Stdout,
+				})
+				if err != nil {
+					fmt.Println(err)
+				}
 
 				return nil
 			},
